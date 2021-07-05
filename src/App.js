1eed8f36b4e3f0e53cba50c5a9bb4fb16b80549f
unknown <ammarasc74@gmail.com>
2021-07-05 08:14:56 +0200

    add message form, login form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import { ChatEngine } from "react-chat-engine";
-import "../src/App.css"
+import { ChatEngine } from 'react-chat-engine';
 
-import ChatFeed from "./components/ChatFeed.jsx";
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
 
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+
     return (
         <ChatEngine
             height="100vh"
-            projectID="3c8f17bd-c377-4bbd-814b-ffbbd7a1e3ed"
-            userName="ammarasc74"
-            userSecret="12345"
+            projectID={projectID}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
         />
     );
-}
+};
+
+// infinite scroll, logout, more customizations...
+
 export default App;
--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -8,32 +8,40 @@ const ChatFeed = (props) => {
 
     const chat = chats && chats[activeChat];
 
+    const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
+        <div
+            key={`read_${index}`}
+            className="read-receipt"
+            style={{
+                float: isMyMessage ? 'right' : 'left',
+                backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
+            }}
+        />
+    ));
+
     const renderMessages = () => {
         const keys = Object.keys(messages);
 
-        return keys.map((key, i) => {
+        return keys.map((key, index) => {
             const message = messages[key];
-            const lastMessageKey = i === 0 ? null : keys[i - 1];
+            const lastMessageKey = index === 0 ? null : keys[index - 1];
             const isMyMessage = userName === message.sender.username;
 
             return (
-                <div key={`msg_${i}`} style={{ width: '100%' }}>
+                <div key={`msg_${index}`} style={{ width: '100%' }}>
                     <div className="message-block">
                         {isMyMessage
                             ? <MyMessage message={message} />
                             : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />}
                     </div>
-                    <div className="read-receipts"
-                        style={{
-                            marginRight: isMyMessage ? '18px' : '0px',
-                            marginLeft: isMyMessage ? '0px' : '68px'
-                        }}>
-                        read-receipts
+                    <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+                        {renderReadReceipts(message, isMyMessage)}
                     </div>
                 </div>
             );
-        })
-    }
+        });
+    };
+
 
     if (!chat) return " Loading....";
 
